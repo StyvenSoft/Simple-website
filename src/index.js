ed1772df186b80ac55e7e48005d1bacd072b081b
Steveen <steveen.echeverri@gmail.com>
2020-03-13 19:28:43 -0500

    add change settings template engine

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ const path = require('path')
 
 // settings
 app.set('port', 4000);
+app.set('views', path.join(__dirname, 'views'));
+// Template Engine
+app.set('view engine', 'ejs');
 
 // routers
 app.get('/', (req, res) => {
     //res.send('Hello World')
-    res.sendFile(path.join(__dirname, 'views/index.html'))
+    //res.sendFile(path.join(__dirname, 'views/index.html'))
+    res.render('index');
 });
 
 // listener the server
